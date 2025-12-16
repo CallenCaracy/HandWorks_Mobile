e728eb0d750ba815e2d5b01b46976eaa6b38c428
CallenCaracy <diangcocalen@gmail.com>
2025-12-16 19:34:29 +0800

    Learn More page is done continuing with connection manager next

--- app/src/main/java/handworks_cleaning_service/handworks_mobile/ui/pages/index/LearnMore.java
+++ app/src/main/java/handworks_cleaning_service/handworks_mobile/ui/pages/index/LearnMore.java
@@ -1,6 +1,7 @@
 package handworks_cleaning_service.handworks_mobile.ui.pages.index;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +10,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import handworks_cleaning_service.handworks_mobile.R;
+import handworks_cleaning_service.handworks_mobile.ui.pages.auth.Login;
+import handworks_cleaning_service.handworks_mobile.utils.NavigationUtil;
 
 public class LearnMore extends AppCompatActivity {
+    public Button getStarted, backToLanding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,11 @@ public class LearnMore extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        getStarted = findViewById(R.id.btnGoToLogIn);
+        backToLanding = findViewById(R.id.btnGoBackToLanding);
+
+        getStarted.setOnClickListener(v -> NavigationUtil.navigateTo(this, Login.class));
+        backToLanding.setOnClickListener(v -> NavigationUtil.navigateTo(this, LandingPage.class));
     }
 }
